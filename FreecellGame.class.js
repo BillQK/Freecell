7678f23e10c31e34afd5fcaed894bb84d20f011c
BillQK <billngo47@gmail.com>
2022-05-04 07:55:40 -0400

    First Commit

--- FreecellGame.class.js
+++ FreecellGame.class.js
@@ -47,23 +47,55 @@ class FreecellGame {
   // to destPile, e.g. {type:"open", index: 3}
   // mutates the game state.
   executeMove(srcPile, destPile) {
-    let src = this.getPile(srcPile.type);
-    let dest = this.getPile(destPile.type);
-    let pile =  dest[destPile.index]; 
-    let lastCard = pile[pile.length -1];
+    this._addCard(destPile, srcPile);
+    this._removeCard(srcPile);
 
-    if (this.isValidMove(srcPile, destPile)   
-      && srcPile.cardIndex < src.length
-      && this.isBuild(srcPile.index, srcPile.cardIndex)
-      && FreecellGame._isStackable(
-        lastCard,
-        src[srcPile.index][srcPile.cardIndex])) {
-      for (let i = srcPile.cardIndex; i > src.length; i++) {
-        dest[destPile.index].push(src[srcPile.index][i]);
-      }
-      src[srcPile.index].splice(srcPile.cardIndex);
+  }
+
+  _removeCard(srcPile) {
+    let src = null;
+    let srcSeq = null;
+    switch (srcPile.type) {
+      case "cascade":
+        src = this._getPile("cascade")[srcPile.index];
+        srcSeq = src.slice(srcPile.cardIndex);
+        for (let card in srcSeq) {
+          src.pop();
+        }
+        break;
+      case "open":
+        src = this._getPile("open")[srcPile.index];
+        srcSeq = new Array(src[0]);
+        src.pop();
+        break;
+      case "foundation":
+        return;
+      default:
+        return;
     }
+  }
 
+  _addCard(destPile, srcPile) {
+    let dest = null;
+    let srcCards = this._getPile(srcPile.type)[srcPile.index].slice(srcPile.cardIndex);
+    switch (destPile.type) {
+      case "cascade":
+        dest = this._getPile("cascade")[destPile.index];
+        for (let i = 0; i < srcCards.length; i++) {
+          dest.push(new Card(srcCards[i]));
+        }
+        break;
+      case "open":
+        dest = this._getPile("open")[destPile.index];
+        dest.push(srcCards[0]);
+        break;
+      case "foundation":
+        dest = this._getPile("foundation")[destPile.index];
+        dest.push(srcCards[0]);
+        break;
+      default:
+        break;
+    }
   }
 
 
@@ -79,19 +111,55 @@ class FreecellGame {
   // return index of first valid foundation destination for the given card,
   // or anything else if there is no valid foundation destination
   getValidFoundation(srcPile) {
+    let src = null;
+    // get src Card
+    switch (srcPile.type) {
+      case "cascade":
+        src = this._getPile("cascade")[srcPile.index][srcPile.cardIndex];
+        break;
+      case "open":
+        src = this._getPile("open")[0];
+        break;
+      case "foundation":
+        return null;
+      default:
+        return null;
+    }
+    // return index of first valid foundation 
+    let fPile = null;
+    for (let i = 0; i < this.foundation.length; i++) {
+      fPile = this._getPile("foundation")[i];
+      if (fPile.length != 0) {
+        if (fPile[fPile.length - 1].value == src.value - 1 && fPile[fPile.length - 1].getSuit() == src.getSuit()) {
+          return i;
+        }
+      } else {
+        if (src.value == 1) {
+          return i;
+        }
+      }
+      
+    }
+    return null;
 
   }
   // return index of first empty open pile
   // or anything else if no empty open pile
   getFirstAvailableOpen() {
-    return 0;
+    for (let a = 0; a < this.getNumOpen(); a++) {
+      let aPile = this.getOpen();
+      if (aPile[a].length == 0) {
+        return a;
+      }
+    }
+    return null;
   }
 
   // return true if in the given cascade pile, starting from given index, there is a valid "build"
   isBuild(pileIdx, cardIdx) {
-    let src = this.getPile("cascade");
+    let src = this._getPile("cascade");
     let sublist = src[pileIdx].slice(cardIdx);
-    return this.validBuild(sublist);
+    return this._validBuild(sublist);
   }
 
   // return true if the move from srcPile to destPile would be valid, false otherwise.
@@ -102,22 +170,80 @@ class FreecellGame {
       || srcPile.type == "foundation") {
       return false;
     }
-    // all the rules for moves in freecell:
-    if (srcPile.index < 0 || srcPile.index >= this.getPile(srcPile.type).length) {
-      throw "Invalid source pile number!";
-    } else if (destPile.index < 0 || destPile.index >= this.getPile(destPile.type).length) {
-      throw "Invalid destination pile number";
-    } else {
-      return true;
+    if (sSeq.length > this._numCanMove(destPile)) {
+      return false;
     }
-  }
-
-  validBuild(sublist) {
-    if (sublist.length > 1) {
-      for (let i = sublist.length - 1; i > 1; i--) {
-        if (!FreecellGame._isStackable(sublist[i - 1], sublist[i])) {
+    // all the rules for moves in freecell:
+    let src = null;
+    let sSeq = null;
+    // Get
+    switch (srcPile.type) {
+      case "cascade":
+        src = this.getCascade()[srcPile.index];
+        sSeq = src.slice(srcPile.cardIndex);
+        if (!this.isBuild(srcPile.index, srcPile.cardIndex)) {
           return false;
         }
+        break;
+      case "open":
+        src = this.getOpen()[srcPile.index];
+        sSeq = new Array(src[0]);
+        break;
+      case "foundation":
+        return false;
+      default:
+        return false;
+    }
+
+    // Add
+    let dest = null;
+    switch (destPile.type) {
+      case "cascade":
+        dest = this.getCascade()[destPile.index];
+        if (dest.length != 0) {
+          let lastCard = dest[dest.length - 1];
+          let firstCard = sSeq[0];
+          if (!FreecellGame._isStackable(firstCard, lastCard)) {
+            return false;
+          }
+        }
+        break;
+      case "open":
+        dest = this.getOpen()[destPile.index];
+        if (dest.length != 0) {
+          return false;
+        }
+        if (sSeq.length != 1) {
+          return false;
+        }
+        break;
+      case "foundation":
+        if (destPile.index == -1) {
+          return false;
+        }
+        dest = this.getFoundation()[destPile.index];
+        if (sSeq.length != 1) {
+          return false;
+        }
+        if (dest.length > 13) {
+          return false;
+        }
+        break;
+      default:
+        return false;
+    }
+    return true;
+  }
+
+  _validBuild(sublist) {
+    let lastCard = sublist[0]
+    if (sublist.length > 1) {
+      for (let i = 1; i < sublist.length; i++) {
+        if (!FreecellGame._isStackable(sublist[i], lastCard)) {
+          return false;
+        } 
+        lastCard = sublist[i]; 
+
       }
       return true;
     } else {
@@ -126,7 +252,8 @@ class FreecellGame {
 
   }
 
-  getPile(type) {
+  // return the actual list, not a copy
+  _getPile(type) {
     switch (type) {
       case "foundation":
         return this.foundation;
@@ -143,6 +270,30 @@ class FreecellGame {
   _numCanMove(destPileIndex) {
     // computer number of open piles, number of empty cascade piles
     // apply the formula from Assignment 3
+    // computer number of open piles, number of empty cascade piles
+    // apply the formula from Assignment 3
+    let n = 0;
+    let k = 0;
+    for (let i = 0; i < this.getNumOpen(); i++) {
+      let pile = this.getOpen()[i];
+      if (pile.length == 0) {
+        n += 1;
+      }
+    }
+    for (let i = 0; i < this.getNumCascade(); i++) {
+      let pile = this.getCascade()[i];
+      if (pile.length == 0) {
+        k += 1;
+      }
+    }
+    if (destPileIndex.type == "cascade") {
+      if (this.getCascade()[destPileIndex.index].length == 0) {
+        if (n != 0) {
+          n -= 1;
+        }
+      }
+    }
+    return (n + 1) * Math.pow(2, k);
 
     // why do we need destPileIndex? if the destination cascade pile is empty, it doesn't count as an empty cascade in the formula.
 
